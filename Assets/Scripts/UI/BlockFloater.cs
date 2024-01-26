6f38d80cc7b8de0b45366ad5cd3c9d19f0407754
fumi@MSI <f.shibasaki@keio.jp>
2024-01-26 17:46:43 +0900

    floating blocksを毎回ランダムに配置

--- Assets/Scripts/UI/BlockFloater.cs
+++ Assets/Scripts/UI/BlockFloater.cs
@@ -6,19 +6,29 @@ public class BlockFloater : MonoBehaviour
 {
     private Rigidbody rb;
     private Vector3 v = new Vector3(0.0f, 0.0f, 0.0f);
-    private 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        v.x = Random.Range(0, 15);
+        v.y = Random.Range(0, 25);
+        v.z = 0.0f;
+        gameObject.transform.position = v;
+        
+        v.x = Random.Range(0, 360);
+        v.y = Random.Range(0, 360);
+        v.z = Random.Range(0, 360);
+        gameObject.transform.Rotate(v);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        v.x = UnityEngine.Random.Range(0.85f, -0.85f);
-        v.y = UnityEngine.Random.Range(10.645f, 8.98f);
+        v.x = Random.Range(0.85f, -0.85f);
+        v.y = Random.Range(10.645f, 8.98f);
+        v.z = 0.0f;
         rb.AddForce(v, ForceMode.Force);
     }
 
